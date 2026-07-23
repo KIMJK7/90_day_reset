82a13b3e452886e1957ae520fd008fcc8c5618ae
KIMJK7 <123354435+KIMJK7@users.noreply.github.com>
2026-07-23 14:06:33 +0530

    Starting of our 90 day reset journey

---
+++ Week_1/Day_1/Hello.java
@@ -0,0 +1,14 @@
+// pages needs ot be imported on the very top, it acts like the strcture of the file.
+// as project grows this helps organise code logically and prevent name conflict
+
+package Week_1.Day_1;
+
+//everything in java nees to be inside a class and the class name should be same as file name
+public class Hello {
+
+    // main is the starting point of any code
+    // JVM looks for main as a starting point
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java programming!");
+    }
+}
