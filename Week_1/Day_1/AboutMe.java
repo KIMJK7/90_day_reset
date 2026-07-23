7c1a4889a4615a801e19dd44fc2c339dcba4bf2a
KIMJK7 <123354435+KIMJK7@users.noreply.github.com>
2026-07-23 14:28:41 +0530

    commit About Me file

---
+++ Week_1/Day_1/AboutMe.java
@@ -0,0 +1,8 @@
+package Week_1.Day_1;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println(
+                "This 90 day reset bootcamp is to reset the mind to think like an Engineer stop overly reling on AI and actually work on logical and critical thinking skills \n this cource is a self study cource with primary langauge as JAVA \nWith each day the mind will get sharper and stronger.");
+    }
+}
