978eba62b73bb5b24c0c249f8256edea675dda65
KIMJK7 <123354435+KIMJK7@users.noreply.github.com>
2026-07-30 18:36:51 +0530

    Enhance mini project add display menu and delete option

--- Week_1/Day_5/StudentManagementSystem.java
+++ Week_1/Day_5/StudentManagementSystem.java
@@ -4,43 +4,67 @@ import java.util.Scanner;
 
 public class StudentManagementSystem {
 
-    public static void displayMenu(){
+    public static void displayMenu() {
         System.out.println("Menu :");
-        System.out.println("1. Display Students \n2.Add Students \n3.Search By ROll No \n4.Highest Marks \n5. Average Marks.")
+        System.out.println(
+                "1. Display Students \n2.Add Students \n3.Search By ROll No \n4.Highest Marks \n5. Average Marks.\n6.Delete Student Info \n7. Exit");
     }
 
-    public static String addStuName(String name, Scanner sc) {
+    public static void addStuInfo(String[] name, int[] rollno, int[] marks, int i, Scanner sc) {
+
+        name[i] = addStuName(sc);
+        rollno[i] = addStuRollNo(sc);
+        sc.nextLine();
+        marks[i] = addStuMarks(sc);
+        sc.nextLine();
+
+    }
+
+    public static String addStuName(Scanner sc) {
         System.out.println("Enter name of Student:");
-        name = sc.nextLine();
+        String name = sc.nextLine();
         return name;
     }
 
-    public static int addStuRollNo(int Rollno, Scanner sc) {
+    public static int addStuRollNo(Scanner sc) {
         System.out.println("Enter RollNo of Student:");
-        Rollno = sc.nextInt();
+        int Rollno = sc.nextInt();
         return Rollno;
     }
 
-    public static int addStuMarks(int marks, Scanner sc) {
+    public static int addStuMarks(Scanner sc) {
         System.out.println("Enter marks Student:");
-        marks = sc.nextInt();
+        int marks = sc.nextInt();
         return marks;
     }
 
-    public static void display(String[] names, int[] Rollno, int[] marks) {
+    public static void display(String[] names, int[] Rollno, int[] marks, int count) {
         System.out.println("names \t Rollno \t marks ");
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < count; i++) {
             System.out.println(names[i] + "\t" + Rollno[i] + "\t" + marks[i]);
         }
     }
 
-    public static int searchStuByRollNo(int[] Rollno, Scanner sc) {
+    public static void deleteStuInfo(String[] name, int[] Rollno, int[] mark, int count, int index) {
+
+        for (int i = index; i < count - 1; i++) {
+            name[i] = name[i + 1];
+            Rollno[i] = Rollno[i + 1];
+            mark[i] = mark[i + 1];
+        }
+        name[count - 1] = null;
+        Rollno[count - 1] = 0;
+        mark[count - 1] = 0;
+
+    }
+
+    public static int searchStuByRollNo(int[] Rollno, Scanner sc, int count) {
         System.out.println("Enter the RollNO to be searched:");
         int searchNum = sc.nextInt();
 
         int index = -1;
 
-        for (int i = 0; i < Rollno.length; i++) {
+        for (int i = 0; i < count; i++) {
             if (Rollno[i] == searchNum) {
                 index = i;
                 break;
@@ -50,11 +74,14 @@ public class StudentManagementSystem {
         return index;
     }
 
-    public static int highestMarks(int[] marks) {
-        int stu = marks.length;
+    public static int highestMarks(int[] marks, int count) {
+        if (count == 0) {
+            System.out.println("Enter Student Info First");
+            return 0;
+        }
         int index = 0;
         int max = marks[0];
-        for (int i = 1; i < stu; i++) {
+        for (int i = 1; i < count; i++) {
             if (max < marks[i]) {
                 max = marks[i];
                 index = i;
@@ -66,64 +93,90 @@ public class StudentManagementSystem {
 
     }
 
-    public static void averageMarks(int[] marks) {
-        int stu = marks.length;
+    public static void averageMarks(int[] marks, int count) {
 
         int total = 0;
-        for (int i = 0; i < stu; i++) {
+        for (int i = 0; i < count; i++) {
             total += marks[i];
         }
-        double avg = (double) total / stu;
-        System.out.println("Avg marks are " + avg);
+        if (count != 0) {
+            double avg = (double) total / count;
+            System.out.println("Avg marks are " + avg);
+        } else {
+            System.out.println("Enter Student Info before Choosing this option");
+        }
+
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // Requirements:
 
-        // Store for 10 students:
-
-        // Name
-        // Roll Number
-        // Marks
         String[] name = new String[10];
         int[] rollno = new int[10];
         int[] marks = new int[10];
-        // Features:
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Enter Details Name, roll no and marks for Student" + (i + 1));
-            name[i] = addStuName(name[i], sc);
-            rollno[i] = addStuRollNo(rollno[i], sc);
-            sc.nextLine();
-            marks[i] = addStuMarks(marks[i], sc);
-            sc.nextLine();
-        }
-        // Add student
 
-        // Display all students
-        display(name, rollno, marks);
-        // Search student by roll number
-        int index = searchStuByRollNo(rollno, sc);
-        if (index != -1) {
-            System.out.println("Name: " + name[index]);
-            System.out.println("RollNo: " + rollno[index]);
-            System.out.println("Marks: " + marks[index]);
-        } else {
-            System.out.println("Not found");
-        }
-        // Find highest marks
-        int markIndex = highestMarks(marks);
-        System.out.println("Name : " + name[markIndex]);
-        System.out.println("Rollno : " + rollno[markIndex]);
+        int count = 0;
+        int option;
+        boolean running = true;
+        do {
+            // Display menu design:
+            displayMenu();
+            System.out.println("Choose any option:");
+            option = sc.nextInt();
 
-        // Find average marks
-        averageMarks(marks);
+            switch (option) {
+                case 1:
+                    display(name, rollno, marks, count);
+                    break;
+                case 2:
+                    if (count < 10) {
+                        addStuInfo(name, rollno, marks, count, sc);
+                        count++;
+                    } else {
+                        System.out.println("Student Reaached max capacity");
+                    }
 
-        // Use arrays only (no ArrayList yet).
+                    break;
+                case 3:
+                    int indexSearch = searchStuByRollNo(rollno, sc, count);
+                    if (indexSearch != -1) {
+                        System.out.println("Name: " + name[indexSearch]);
+                        System.out.println("RollNo: " + rollno[indexSearch]);
+                        System.out.println("Marks: " + marks[indexSearch]);
+                    } else {
+                        System.out.println("Not found");
+                    }
+                    break;
+                case 4:
+                    int markIndex = highestMarks(marks, count);
+                    System.out.println("Name : " + name[markIndex]);
+                    System.out.println("Rollno : " + rollno[markIndex]);
+                    break;
+                case 5:
+                    averageMarks(marks, count);
+                    break;
+                case 6:
 
-        // Display menu design:
-        displayMenu();
-        System.out.println("Choose any option:");
+                    System.out.println("Enter the Student Rollno to delete:");
+                    int position = searchStuByRollNo(rollno, sc, count);
+                    if (position != -1) {
+                        deleteStuInfo(name, rollno, marks, count, position);
+                        count--;
+                    } else {
+                        System.out.println("Student not found.");
+                    }
+
+                    break;
+
+                case 7:
+                    System.out.println("Exit");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid");
+
+            }
+        } while (running);
 
         sc.close();
     }
