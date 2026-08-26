a5a8f900af881b981861f54f401bef4f73b713a4
KIMJK7 <123354435+KIMJK7@users.noreply.github.com>
2026-08-26 14:51:11 +0530

    leet code 1011 and 875

--- Week_4/binarySeasrch.java
+++ Week_4/binarySeasrch.java
@@ -87,11 +87,103 @@ public class binarySeasrch {
         return ans;
     }
 
+    // Problem
+
+    // You have:
+
+    // weights = [1,2,3,4,5,6,7,8,9,10]
+
+    // A ship must transport all packages in 5 days.
+
+    // Packages must be loaded in order.
+
+    // What is the minimum ship capacity required?
+    static boolean isPossible(int[] nums, int days, int value) {
+
+        int sum = 0;
+        int daysUsed = 1;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > value) {
+                sum = nums[i];
+                daysUsed++;
+            }
+        }
+        if (daysUsed <= days) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static int minShipCapacity(int[] nums, int days) {
+        int minCap = Integer.MAX_VALUE;
+        // we are assuming that array is sorted:
+        // left and right are not index but values;
+        // int left = nums[nums.length - 1];
+        // if array is not sorted:
+        int left = 0;
+        for (int num : nums) {
+            left = Math.max(left, num);
+        }
+        int right = 0;
+        for (int num : nums) {
+            right += num;
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isPossible(nums, days, mid)) {
+                minCap = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+
+        return minCap;
+    }
+
+    static boolean isPossiblehours(int[] piles, int h, int speed) {
+
+        long hoursUsed = 0;
+
+        for (int pile : piles) {
+            hoursUsed += ((long) pile + speed - 1) / speed;
+        }
+
+        return hoursUsed <= h;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+        int ans = 0;
+        int left = 1;
+
+        int right = 0;
+        for (int num : piles) {
+            right = Math.max(right, num);
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isPossiblehours(piles, h, mid)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 2, 2, 4, 5 };
-        int indexFirst = firstOccurrence(nums, 2);
-        System.out.println("index : " + indexFirst);
-        int indexLast = lastOccurrence(nums, 2);
-        System.out.println("index : " + indexLast);
+        // int[] nums = { 1, 2, 2, 2, 4, 5 };
+        // int indexFirst = firstOccurrence(nums, 2);
+        // System.out.println("index : " + indexFirst);
+        // int indexLast = lastOccurrence(nums, 2);
+        // System.out.println("index : " + indexLast);
+        int[] weights = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int days = 5;
+        int minCapacity = minShipCapacity(weights, days);
+        System.out.println("Minimun capacity required: " + minCapacity);
     }
 }
