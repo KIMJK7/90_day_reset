b812660916c7af73cb67f492563b2e1436c3b120
KIMJK7 <123354435+KIMJK7@users.noreply.github.com>
2026-09-15 10:32:49 +0530

    slow and fast poniter

---
+++ Week_5/Day_2/middleLL.java
@@ -0,0 +1,27 @@
+package Week_5.Day_2;
+
+public class middleLL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class LinkList {
+        Node head;
+
+        Node middleNode() {
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
